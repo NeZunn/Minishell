92fff4c38bfb121b0a3a1dd9e833c01a8ec1a6d7
Lucas <129052371+Deng-Yi-Jin@users.noreply.github.com>
2023-08-17 11:07:05 +0000

    redo check_type and seperator

--- srcs/lexer/lexer_utils.c
+++ srcs/lexer/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 21:11:33 by djin              #+#    #+#             */
-/*   Updated: 2023/08/17 08:08:09 by codespace        ###   ########.fr       */
+/*   Updated: 2023/08/17 11:04:01 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,91 +18,22 @@ void	space_skip(char *str, int *i)
 		(*i)++;
 }
 
-char	check_type(char *str, t_token tokens)
+char	check_type(char *str, t_token *tokens)
 {
 	int	i;
 
 	i = 0;
 	while(str[i])
 	{
-		is_seperator(str, &i, &tokens);
-		is_seperator2(str, &i, &tokens);
-		keyword(str, &i, &tokens);
-	}
-}
-
-
-
-void	keyword(char *str, int *i, t_token *tok)
-{
-	if (str[*i] == '$')
-	{
-		tok->type = DOLLAR_SIGN;
-		(*i)++;
-	}
-}
-
-void	is_seperator(char *str, int *i, t_token *tok)
-{
-	if (str[*i] == '|')
-	{
-		tok->type = PIPE;
-		(*i)++;
-	}
-	if (str[*i] == '>')
-	{
-		tok->type = REDIR_OUT;
-		(*i)++;
-	}
-	if (str[*i] == '<')
-	{
-		if (str[*i + 1] == '<')
-		{
-			tok->type = HERE_DOC;
-			(*i) += 2;
-		}
-		else
-		{
-			tok->type = REDIR_IN;
-			(*i)++;
-		}
-	}
-	if (str[*i] == '>')
-	{
-		if (str[*i + 1] == '>')
-		{
-			tok->type = REDIR_OUT_APPEND;
-			(*i) += 2;
-		}
-		else
-		{
-			tok->type = REDIR_IN;
-			(*i)++;
-		}
-	}
-}
-
-void	is_seperator2(char *str, int *i, t_token *tok)
-{
-		if (str[*i] == '&')
-	{
-		if (str[*i + 1] == '&')
-		{
-			tok->type = DOUBLE_AND;
-			(*i) += 2;
-		}
-		else
-		{
-			tok->type = AND;
-			(*i)++;
-		}
-	}
-	if (str[*i] == '(' || str[*i] == ')')
-	{
-		if (str[*i] == '(')
-			tok->type = OPEN_BRACKET;
-		else
-			tok->type = CLOSE_BRACKET;
-		(*i)++;
+		if (str[i] == ' ')
+			space_skip(str, &i);
+		if (str[i] == '|' || (str[i] == '>' && str[i + 1] == '>') || str[i] == '<'
+			|| (str[i] == '<' && str[i + 1] == '<') || str[i] == '>' || str[i] == '&'
+			|| str[i] == '(' || str[i] == ')' || str[i] == ft_isdigit(str[i])
+			|| str[i] == '$' || str[i] == '\'' || str[i] == '\"'|| str[i] == '\\'
+			|| str[i] == '/' || (str[i] == '|' && str[i + 1] == '|') || str[i] == '{'
+			|| str[i] == '}')
+			is_seperator(str, &i, &tokens);
+		i++;
 	}
 }
